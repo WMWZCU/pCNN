fc22083a6b855233901363939b615aac94b05d99
WMWZCU <w.zhao@pku.edu.cn>
2017-02-17 12:10:14 -0700

    Update generate_dataset.m

--- generate_dataset.m
+++ generate_dataset.m
@@ -3,6 +3,8 @@ load data/gt_half_half.mat; % load the original image
 load data/image_half_half.mat; % load the reference map
 im = image_half_half;
 gt = gt_half_half;
+tr_num = 0.2;
+val_num=0.2;
 % Begain to generate samples
 temp_blob=[];
 temp_label = [];
@@ -11,6 +13,11 @@ count = 1;
 
 for i = 1:size(unique(gt),1) 
     [temp_x, temp_y] = find(gt==i);
+    total_num = tr_num+val_num;
+    num_total = fix(total_num*size(temp_x,1));
+    idx_tr =  randint(1,num_total,[1,size(temp_x,1)]);
+    temp_x = temp_x(idx_tr);
+    temp_y = temp_y(idx_tr);
     for j=1:size(temp_x,1)
             if temp_x(j)-9<1 
                 temp_x(j) =10;
@@ -30,11 +37,11 @@ for i = 1:size(unique(gt),1)
             temp_label(count)=i;
             count = count+1;
     end   
-    temp_set = ones(1,size(temp_x,1));
+    temp_set = ones(1,size(temp_x,1))*3;
     % number of validation samples or training samples
-    num_val = fix(0.8*size(temp_x,1));
-    idx =  randint(1,num_val,[1,size(temp_x,1)]);
-    temp_set(idx) = 3;
+    num_tr = fix(tr_num/(total_num)*size(temp_x,1));
+    idx_tr =  randint(1,num_tr,[1,size(temp_x,1)]);
+    temp_set(idx_tr) = 1;
     set = cat(2,set,temp_set);
 end
 data = single(reshape(temp_blob',18,18,3,[]));
